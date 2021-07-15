aca659ee145617cc5936fbed92c4a6d154e8a392
MichelAndrew <michel.dasilva@mercadolivre.com>
2021-07-15 10:40:02 -0300

    US0002: Metodo e testes criados

--- src/main/java/br/com/meli/desafio_quality/service/HouseService.java
+++ src/main/java/br/com/meli/desafio_quality/service/HouseService.java
@@ -4,6 +4,8 @@ import br.com.meli.desafio_quality.model.House;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 public class HouseService {
 
@@ -12,5 +14,9 @@ public class HouseService {
 
   public double getTotalArea(House house) {
     return house.getRooms().stream().mapToDouble(r -> roomService.getRoomArea(r)).sum();
+  }
+
+  public BigDecimal calculateHousePrice (House house){
+    return house.getDistrict().getValue().multiply(BigDecimal.valueOf(getTotalArea(house)));
   }
 }
--- src/test/java/br/com/meli/desafio_quality/service/HouseServiceTest.java
+++ src/test/java/br/com/meli/desafio_quality/service/HouseServiceTest.java
@@ -47,4 +47,32 @@ public class HouseServiceTest {
 
     assertNotEquals(27, houseService.getTotalArea(house));
   }
+
+  @Test
+  public void getHousePrice_mustPass(){
+    List<Room> rooms = new ArrayList<>();
+    rooms.add(new Room("Bedroom", 2d, 3d));
+    rooms.add(new Room("Kitchen", 4d, 2d));
+    rooms.add(new Room("Living Room", 3d, 3d));
+
+    District district = new District("Pirituba City", new BigDecimal(2000));
+
+    House house = new House("Patrícia", district, rooms);
+
+    assertEquals(new BigDecimal("46000.0"),houseService.calculateHousePrice(house));
+  }
+
+  @Test
+  public void getHousePrice_mustNotPass(){
+    List<Room> rooms = new ArrayList<>();
+    rooms.add(new Room("Bedroom", 2d, 3d));
+    rooms.add(new Room("Kitchen", 4d, 2d));
+    rooms.add(new Room("Living Room", 3d, 3d));
+
+    District district = new District("Pirituba City", new BigDecimal(2000));
+
+    House house = new House("Patrícia", district, rooms);
+
+    assertNotEquals(new BigDecimal("49000.0"),houseService.calculateHousePrice(house));
+  }
 }
